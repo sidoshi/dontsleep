fd94d0c306937ebb34e4d576baf2e4e23b56f887
Siddharth Doshi <siddharth.doshi@freighthub.com>
2021-04-14 11:15:28 +0530

    Compatible for always running in bg

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,7 @@ use mouse_rs::Mouse;
 use std::time::Duration;
 use tokio::time::sleep;
 
-const POINT_A: i32 = 50;
-const POINT_B: i32 = 350;
+const MOVEMENT: i32 = 1;
 
 fn get_position(mouse: &Mouse) -> (i32, i32) {
     let point = mouse
@@ -13,34 +12,32 @@ fn get_position(mouse: &Mouse) -> (i32, i32) {
     (point.x as i32, point.y as i32)
 }
 
-fn mouse_is_on_point_a(mouse: &Mouse) -> bool {
-    let position = get_position(&mouse);
-    position == (POINT_A, POINT_A)
+fn move_left(mouse: &Mouse) {
+    let (x, y) = get_position(&mouse);
+    let x = x - MOVEMENT;
+    println!("Moving the mouse to the left");
+    mouse.move_to(x, y).expect("Error moving mouse to the left");
 }
 
-fn move_to_point_a(mouse: &Mouse) {
-    println!("Moving the mouse to point A");
+fn move_right(mouse: &Mouse) {
+    let (x, y) = get_position(&mouse);
+    let x = x + MOVEMENT;
+    println!("Moving the mouse to the right");
     mouse
-        .move_to(POINT_A, POINT_A)
-        .expect("Error moving mouse to Point A");
-}
-
-fn move_to_point_b(mouse: &Mouse) {
-    println!("Moving the mouse to point B");
-    mouse
-        .move_to(POINT_B, POINT_B)
-        .expect("Error moving mouse to Point B");
+        .move_to(x, y)
+        .expect("Error moving mouse to the right");
 }
 
 async fn setup() {
     let mouse = Mouse::new();
     loop {
-        sleep(Duration::from_secs(3)).await;
-
-        if mouse_is_on_point_a(&mouse) {
-            move_to_point_b(&mouse);
-        } else {
-            move_to_point_a(&mouse);
+        for i in 1..=10 {
+            if i % 2 == 0 {
+                move_left(&mouse);
+            } else {
+                move_right(&mouse);
+            }
+            sleep(Duration::from_secs(60 * 10)).await;
         }
     }
 }
